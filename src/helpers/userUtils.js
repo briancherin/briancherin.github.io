9c23c11f2ab0c2c8dd9e8470135f80f460944f3b
Brian Cherin <brian.cherin@gmail.com>
2026-01-10 19:31:15 -0500

    Add breakcrumbs (folder location) to top of notes

--- src/helpers/userUtils.js
+++ src/helpers/userUtils.js
@@ -1,7 +1,40 @@
 // Put your computations here.
 
+function getNoteFolderPathParts(data) {
+  if (!data || !data.page || !data.page.filePathStem) {
+    return [];
+  }
+
+  if (data.tags && data.tags.indexOf("gardenEntry") !== -1) {
+    return [];
+  }
+
+  let pathParts = [];
+  if (data["dg-path"]) {
+    pathParts = data["dg-path"].split("/");
+  } else {
+    const stem = data.page.filePathStem;
+    const stemParts = stem.split("notes/");
+    if (stemParts.length < 2) {
+      return [];
+    }
+    pathParts = stemParts[1].split("/");
+  }
+
+  if (pathParts.length <= 1) {
+    return [];
+  }
+
+  pathParts.pop();
+  return pathParts.filter(Boolean);
+}
+
 function userComputed(data) {
-  return {};
+  const noteFolderPathParts = getNoteFolderPathParts(data);
+  return {
+    noteFolderPathParts,
+    noteFolderPath: noteFolderPathParts.join(" > "),
+  };
 }
 
 exports.userComputed = userComputed;
